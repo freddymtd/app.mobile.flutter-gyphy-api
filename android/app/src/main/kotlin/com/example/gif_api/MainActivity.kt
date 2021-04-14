26e42ca4873e1cc76d97dfcc2cc02776bba247e9
freddymtd <freddy-zenteno@hotmail.com>
2021-04-14 15:25:20 -0300

    projeto base

---
+++ android/app/src/main/kotlin/com/example/gif_api/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gif_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
